064e479321fd8f4814b8fd3906bb48bcfcb15b31
Taras  Batyuchok <57962357+TarasBatyuchok@users.noreply.github.com>
2023-02-03 17:43:34 +0200

    auth middlewar

--- server/index.js
+++ server/index.js
@@ -2,18 +2,21 @@ import  express  from "express";
 import bodyParser from "body-parser";
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js'
 
 
 const app = express();
-// dotenv.config();
+
+
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://javascriptmastery:299792458@cluster0.ope7jh3.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
